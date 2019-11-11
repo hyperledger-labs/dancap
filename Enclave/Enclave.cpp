499546aa31f8d85c38d493f46cca367aac3b696a
Dan Middleton <dan.middleton@intel.com>
2019-11-11 13:35:18 +0200

    Remove Platform Services functions
    Make simplified enclave with no PSE requirements. This eliminates
    trusted time and trusted counters.
    
    Signed-off-by: Dan Middleton <dan.middleton@intel.com>

--- Enclave/Enclave.cpp
+++ Enclave/Enclave.cpp
@@ -1,14 +1,9 @@
 #include "stdio.h"
 #include "Enclave_t.h"
 #include "sgx_trts.h"
-#include "sgx_tae_service.h"
 
 int secret_num = 0;
 bool initialized = false;
-sgx_mc_uuid_t mcid;
-uint32_t mc=0;
-sgx_time_t timer_start;
-sgx_time_source_nonce_t nonce;
 
 #define TIME_LIMIT 30    //~seconds
 #define ATTEMPT_LIMIT 8
@@ -22,23 +17,13 @@ bool init()
     if(ret != SGX_SUCCESS){oPrintStatus(ret); return false;}
     secret_num = (int) rand;
 
-    ret = sgx_create_pse_session();
-    if(ret != SGX_SUCCESS){oPrintStatus(ret); return false;}
-    
-    ret = sgx_create_monotonic_counter(&mcid, &mc); 
-    if(ret != SGX_SUCCESS){oPrintStatus(ret); return false;}
-
-    ret = sgx_get_trusted_time(&timer_start, &nonce);
-    if(ret != SGX_SUCCESS){oPrintStatus(ret); return false;}
-
-    initialized = true;
+   initialized = true;
     return true; 
 }
 
 void ecall_Guess(int guess, int *response)
 {
     sgx_status_t ret = SGX_SUCCESS;
-    sgx_time_t now = 0;
 
     *response = 100; //initialization failure
     if( not initialized){
@@ -47,24 +32,7 @@ void ecall_Guess(int guess, int *response)
         }
     }
 
-    *response = 110; //increment counter failed
-    ret = sgx_increment_monotonic_counter(&mcid, &mc);
-    if(ret != SGX_SUCCESS){oPrintStatus(ret); return;}
-
-    *response = 120; //get time failed
-    ret = sgx_get_trusted_time(&now, &nonce);
-    if(ret != SGX_SUCCESS){oPrintStatus(ret); return;}
-
-    *response = 130; //Attempt limit exceeded
-    if(mc > ATTEMPT_LIMIT) {return;}
-
-    *response = 140; //Spacetime anomaly check Tardis diagnostics
-    if(now < timer_start) {return;}
-
-    *response = 150; //Time expired
-    if(now - timer_start > TIME_LIMIT) {return;}
-
-    *response = 160; //Unknown error
+   *response = 160; //Unknown error
     if(guess < secret_num){ 
         *response = -1;
     } else if( guess == secret_num) { 
--- Makefile
+++ Makefile
@@ -32,7 +32,7 @@
 ######## SGX SDK Settings ########
 
 SGX_SDK ?= /opt/intel/sgxsdk
-SGX_MODE ?= HW
+SGX_MODE ?= SIM 
 SGX_ARCH ?= x64
 SGX_DEBUG ?= 1
 
