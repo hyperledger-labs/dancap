89bc61178f9938d0364d7f7fcf5dd78af02c5d43
Dan Middleton <dan.middleton@intel.com>
2020-01-25 14:37:06 -0600

    Add missing return check in enclave
    Signed-off-by: Dan Middleton <dan.middleton@intel.com>

--- Enclave/Enclave.cpp
+++ Enclave/Enclave.cpp
@@ -4,8 +4,8 @@
 
 int ecall_Hello(sgx_target_info_t* targetInfo, sgx_report_t* report)
 {
-    sgx_status_t ret = SGX_SUCCESS;
-    sgx_create_report(targetInfo, NULL, report);
+    sgx_status_t ret = SGX_ERROR_UNEXPECTED;
+    ret = sgx_create_report(targetInfo, NULL, report);
     if(ret != SGX_SUCCESS){ oPrintStatus(ret);}
     return ret;
 }
