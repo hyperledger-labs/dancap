0b48cd011c295fddc391066204fae251ee0cddf7
Dan Middleton <dan.middleton@intel.com>
2020-04-09 17:32:17 -0500

    Add partial supplemental data output
    Signed-off-by: Dan Middleton <dan.middleton@intel.com>

--- RelyingParty/Verifier.cpp
+++ RelyingParty/Verifier.cpp
@@ -2,8 +2,20 @@
 // SPDX-License-Identifier: Apache-2.0
 #include <iostream>
 #include <fstream>
-#include <time.h>
+//#include <time.h>
+#include <ctime>
 #include "sgx_dcap_quoteverify.h"
+
+void PrintVerificationDetails(uint8_t *supplemental_data){
+    using namespace std;
+    sgx_ql_qv_supplemental_t *data = (sgx_ql_qv_supplemental_t *) supplemental_data;
+
+    cout << endl << "SUPPLEMENTAL DATA DETAILS: " << endl;
+    cout << "Earliest Expiration: " << ctime(&data->earliest_expiration_date);
+    cout << "TCB Level Date Tag : " << ctime(&data->tcb_level_date_tag);
+    cout << "TCB Eval Ref Num   : " << dec << data->tcb_eval_ref_num << endl;
+    cout << endl;
+}
 
 int main() {
     using namespace std;
@@ -31,14 +43,27 @@ int main() {
     quote_file.close();
 
 
-    // verify quote
-
+    // setup to verify quote
     time_t now = time(NULL);
     uint32_t expiration_status = 0;
        
     sgx_ql_qv_result_t verification_result = SGX_QL_QV_RESULT_UNSPECIFIED;
     quote3_error_t result = SGX_QL_ERROR_UNEXPECTED;
+    uint32_t data_size;
+    uint8_t *p_supplemental_data;
 
+    result = sgx_qv_get_quote_supplemental_data_size(&data_size);
+    if (result == SGX_QL_SUCCESS && data_size == sizeof(sgx_ql_qv_supplemental_t)) {
+        cout << "Info: sgx_qv_get_quote_supplemental_data_size successfully returned.\n";
+        p_supplemental_data = (uint8_t*)malloc(data_size);
+    }
+    else {
+        cerr << "Error: sgx_qv_get_quote_supplemental_data_size failed:" << hex << result;
+        data_size = 0;
+        exit(1);
+    }
+
+    // verify Quote
     result = sgx_qv_verify_quote( 
             (uint8_t*) quote,       // Quote
             quote_size,             // Quote size
@@ -47,8 +72,8 @@ int main() {
             &expiration_status,     // Receive expiration indication;
             &verification_result,   // Result of verification check
             NULL,                   // Optional Report Info
-            0,                      // Optional supplemental data size
-            NULL                    // Optional supplemental data
+            data_size,              // Optional supplemental data size
+            p_supplemental_data     // Optional supplemental data
             );
 
     if (result != SGX_QL_SUCCESS){
@@ -61,7 +86,8 @@ int main() {
             cout << "\n Verification result code: " << hex << verification_result << endl;
             cout << dec;
     }
- 
+
+    PrintVerificationDetails(p_supplemental_data);
 
     // TODO: Verify report data
 
